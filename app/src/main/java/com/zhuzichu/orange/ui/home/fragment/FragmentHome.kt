4900411e1cea4563bd0a349695c142f002404a78
524787275@qq.com <qaioasd520>
2019-10-10 00:32:25 +0800

    update

--- app/src/main/java/com/zhuzichu/orange/ui/home/fragment/FragmentHome.kt
+++ app/src/main/java/com/zhuzichu/orange/ui/home/fragment/FragmentHome.kt
@@ -1,14 +1,52 @@
 package com.zhuzichu.orange.ui.home.fragment
 
+import android.app.NotificationManager
+import androidx.navigation.NavDeepLinkBuilder
+import com.jakewharton.rxbinding3.view.clicks
+import com.uber.autodispose.android.autoDispose
 import com.zhuzichu.base.base.BaseFragment
 import com.zhuzichu.base.base.ParamModelDefault
 import com.zhuzichu.orange.BR
-import com.zhuzichu.orange.R
 import com.zhuzichu.orange.databinding.FragmentHomeBinding
 import com.zhuzichu.orange.ui.home.viewmodel.ViewModelHome
+import kotlinx.android.synthetic.main.fragment_home.*
+import android.content.Context
+import com.zhuzichu.base.ext.toast
+import com.zhuzichu.base.notify.Notify
+import com.zhuzichu.orange.R
+
 
 class FragmentHome : BaseFragment<ParamModelDefault, FragmentHomeBinding, ViewModelHome>() {
-    override fun setLayoutId(): Int= R.layout.fragment_home
+
+    override fun setLayoutId(): Int = R.layout.fragment_home
 
     override fun bindVariableId(): Int = BR.viewModel
+
+    override fun initView() {
+        super.initView()
+        title.clicks().autoDispose(title).subscribe({
+            "出发了".toast()
+            createNotification()
+        }, {
+            it.printStackTrace()
+        })
+    }
+
+    private fun createNotification() {
+        val pendingIntent = NavDeepLinkBuilder(requireActivity())
+            .setGraph(R.navigation.navigation_main)
+            .setDestination(R.id.fragmentLanguages)
+            .createPendingIntent()
+
+        val contentIntent = Notify.with(requireContext())
+            .content {
+                title = "哈哈哈哈"
+            }.asBuilder().setContentIntent(pendingIntent)
+
+        val notificationManager =
+            requireContext().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        notificationManager.notify(1, contentIntent.build())
+    }
+
 }
